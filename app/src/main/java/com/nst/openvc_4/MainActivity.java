d2de675f9d8e3c6efb24610311dcec704a9475db
A1exsandro <alexsandro.acp@gmail.com>
2023-03-08 15:40:03 -0400

    opencv is loaded

--- app/src/main/java/com/nst/openvc_4/MainActivity.java
+++ app/src/main/java/com/nst/openvc_4/MainActivity.java
@@ -3,8 +3,19 @@ package com.nst.openvc_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity: ","Opencv is loaded");
+        }
+        else {
+            Log.d("MainActivity: ","Opencv failed to load");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
