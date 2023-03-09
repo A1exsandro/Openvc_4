9c4bbcf848c74cb0aa6b9aa03097e45e990dafca
A1exsandro <alexsandro.acp@gmail.com>
2023-03-08 20:16:41 -0400

    changes in layout activity_main.xml

--- app/src/main/java/com/nst/openvc_4/MainActivity.java
+++ app/src/main/java/com/nst/openvc_4/MainActivity.java
@@ -2,8 +2,11 @@ package com.nst.openvc_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -17,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private Button camera_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera_button=findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 }
