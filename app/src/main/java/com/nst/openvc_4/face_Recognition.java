279fa7e38232a8dab3093f00174afa8a2c1e21b5
A1exsandro <alexsandro.acp@gmail.com>
2023-03-11 14:48:07 -0400

    add face_Recognition class

--- app/src/main/java/com/nst/openvc_4/face_Recognition.java
+++ app/src/main/java/com/nst/openvc_4/face_Recognition.java
@@ -1,4 +1,23 @@
 package com.nst.openvc_4;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import org.opencv.objdetect.CascadeClassifier;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.GpuDelegate;
+
+import java.io.IOException;
+
 public class face_Recognition {
+    private Interpreter interpreter;
+    private  int INPUT_SIZE;
+    private int height=0;
+    private int width=0;
+    private GpuDelegate gpuDelegate=null;
+    private CascadeClassifier cascadeClassifier;
+
+    face_Recognition(AssetManager assetManager, Context context, String modelPath, int input_size) throws IOException {
+
+    }
 }
